6eb42bba0c89b80fb399e1abb46808a771aa101a
David Valdivia <d.valdiviamartinez@gmail.com>
2019-10-13 00:30:46 +0200

    Advancing a little

--- flutter/exemples/lib/main.dart
+++ flutter/exemples/lib/main.dart
@@ -2,100 +2,82 @@ import 'package:flutter/material.dart';
 import 'package:exemples/stand.dart';
 
 void main() {
-  runApp(App());
+  runApp(StanpediaApp());
 }
 
-class App extends StatelessWidget {
+class StanpediaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         brightness: Brightness.dark,
+        canvasColor: Colors.indigo,
       ),
-      home: Scaffold(
-        backgroundColor: Colors.white, //Background colour
-        appBar: AppBar(
-          title: Text('Standpedia'),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              flex: 5,
-              child: AspectRatio(
-                aspectRatio: 2/3,
-                child: Image.asset(
-                  stoneFree.image,
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Container(color: Colors.blue),
-            ),
-             Expanded(
-              flex: 3,
-              child: Container(color: Colors.green),
-            ),
-          ],
-        ),
+      home: StandPage(),
+    );
+  }
+}
+
+class StandPage extends StatelessWidget {
+  const StandPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Standpedia'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: _Header(),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(color: Colors.blue),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(color: Colors.green),
+          ),
+        ],
       ),
     );
   }
 }
 
-class ProductAttribute extends StatelessWidget {
-  final String attrib;
-  final int quantity;
-  final String units;
-  final Icon icon;
-
-  ProductAttribute({
-    @required this.attrib,
-    @required this.quantity,
-    @required this.units,
-    @required this.icon,
-  });
+class _Header extends StatelessWidget {
+  const _Header({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 160,
-        height: 70,
-        child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Text(
-                  attrib,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Container(
-                    child: icon,
-                  ),
-                  Container(
-                    child: Text(
-                      '$quantity' + ' $units',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ]));
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            stoneFree.standName,
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.yellow[300],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset(
+              stoneFree.image,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
