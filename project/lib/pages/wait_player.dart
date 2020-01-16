c05028514b81eabf10092ba3dac41949324eebd5
David Valdivia <d.valdiviamartinez@gmail.com>
2020-01-16 01:14:22 +0100

    Fixed bug making that the native return button works as our return button

--- project/lib/pages/wait_player.dart
+++ project/lib/pages/wait_player.dart
@@ -9,6 +9,7 @@ class WaitPlayer extends StatefulWidget {
 
 class _WaitPlayerState extends State<WaitPlayer> {
   DocumentReference ref;
+  AsyncSnapshot lastSnapshot;
   bool closing;
   @override
   void didChangeDependencies() {
@@ -30,61 +31,72 @@ class _WaitPlayerState extends State<WaitPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Lobby')),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: StreamBuilder(
-          stream: ref.snapshots(),
-          builder: (context, snapshot) {
-            if (closing) {
-              return Text('Returning');
-            }
-            return Column(
-              children: <Widget>[
-                Text("Lobby Game: ${snapshot.data['Name']}"),
-                Text("Player 1: ${snapshot.data['P1']}"),
-                Text("Player 2: ${snapshot.data['P2']}"),
-                (snapshot.data['P1'] ==
-                        PlayerSettingsLocalization.of(context).name)
-                    ? RaisedButton(
-                        child: Text("Start"),
-                        onPressed: () {
-                          if (snapshot.data['P2'] != 'Empty') {
-                            ref.updateData({
-                              'Running': true,
-                            });
-                          }
-                        },
-                      )
-                    : Text("Waiting player 1 to Start"),
-                RaisedButton(
-                  child: Text("Exit"),
-                  onPressed: () {
-                    if (!snapshot.data['Full']) {
-                      setState(() {
-                        closing = true;
-                      });
-                    } else {
-                      if (snapshot.data['P1'] ==
-                          PlayerSettingsLocalization.of(context).name) {
-                        ref.updateData({
-                          'P1': snapshot.data['P2'],
-                        });
-                      }
-                      ref.updateData({
-                        'P2': 'Empty',
-                        'Full': false,
-                      });
-                    }
-                    Navigator.of(context).pop(closing);
-                  },
-                ),
-              ],
-            );
-          },
+    return WillPopScope(
+      onWillPop: () async {
+        _exit(context);
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text('Lobby')),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: StreamBuilder(
+            stream: ref.snapshots(),
+            builder: (context, snapshot) {
+              if (closing) {
+                return Text('Returning');
+              }
+              lastSnapshot = snapshot;
+              return Column(
+                children: <Widget>[
+                  Text("Lobby Game: ${snapshot.data['Name']}"),
+                  Text("Player 1: ${snapshot.data['P1']}"),
+                  Text("Player 2: ${snapshot.data['P2']}"),
+                  (snapshot.data['P1'] ==
+                          PlayerSettingsLocalization.of(context).name)
+                      ? RaisedButton(
+                          child: Text("Start"),
+                          onPressed: () {
+                            if (snapshot.data['P2'] != 'Empty') {
+                              ref.updateData({
+                                'Running': true,
+                              });
+                            }
+                          },
+                        )
+                      : Text("Waiting player 1 to Start"),
+                  RaisedButton(
+                    child: Text("Exit"),
+                    onPressed: () {
+                      _exit(context);
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
+  }
+
+  void _exit(BuildContext context) {
+    if (!lastSnapshot.data['Full']) {
+      setState(() {
+        closing = true;
+      });
+    } else {
+      if (lastSnapshot.data['P1'] ==
+          PlayerSettingsLocalization.of(context).name) {
+        ref.updateData({
+          'P1': lastSnapshot.data['P2'],
+        });
+      }
+      ref.updateData({
+        'P2': 'Empty',
+        'Full': false,
+      });
+    }
+    Navigator.of(context).pop(closing);
   }
 }
