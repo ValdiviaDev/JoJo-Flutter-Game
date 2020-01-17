fc3acb4cdcc3584273b8146ee0a23f58593fc2ca
David Valdivia <d.valdiviamartinez@gmail.com>
2020-01-17 17:41:36 +0100

    Scene game mockup

--- project/lib/pages/scene_game.dart
+++ project/lib/pages/scene_game.dart
@@ -37,7 +37,7 @@ class _SceneGameState extends State<SceneGame> {
     return Scaffold(
       appBar: AppBar(title: Text('Game')),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         child: StreamBuilder(
           stream: lobbyRef.snapshots(),
           builder: (context, snapshot) {
@@ -45,22 +45,40 @@ class _SceneGameState extends State<SceneGame> {
               return Center(child: CircularProgressIndicator());
             }
             return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Expanded(
-                  flex: 3,
-                  child: _PlayersAndStats(),
+                  flex: 4,
+                  child: _PlayersPhotos(),
                 ),
                 Expanded(
-                   flex: 1,
-                  child: RaisedButton(
-                    child: Text('End Game'),
-                    onPressed: () {
-                      closing = true;
-                      lobbyRef.updateData({
-                        'Running': false,
-                      });
-                      Navigator.of(context).pop(false);
-                    },
+                  flex: 3,
+                  child: _PlayerStatsPhotos(),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: _GameResults(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text('End Game'),
+                        onPressed: () {
+                          closing = true;
+                          lobbyRef.updateData({
+                            'Running': false,
+                          });
+                          Navigator.of(context).pop(false);
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text('Rematch (TODO)'),
+                        onPressed: () {},
+                      ),
+                    ],
                   ),
                 ),
               ],
@@ -72,46 +90,88 @@ class _SceneGameState extends State<SceneGame> {
   }
 }
 
-class _PlayersAndStats extends StatelessWidget {
-  const _PlayersAndStats({
+class _GameResults extends StatelessWidget {
+  const _GameResults({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Row(
-          children: <Widget>[
-             Expanded(
-              child: Image.asset(
-                "assets/goldexperience.png",
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-            Expanded(
-              child: Image.asset(
-                "assets/killerqueen.png",
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-          ],
+        Text("The winner of the first triangle is a draw"),
+        Text("The winner of the second triangle is a draw"),
+        Text("The winner of the random stat pick is Gold Experience"),
+        Center(
+          child: Text(
+            "1-0",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
         ),
-        Row(
-          children: <Widget>[
-             Expanded(
-              child: Image.asset(
-                "assets/goldexperiencestats.png",
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-            Expanded(
-              child: Image.asset(
-                "assets/killerqueenstats.png",
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-          ],
+        Center(
+          child: Text(
+            "Gold Experience WINS",
+            style: TextStyle(
+                fontSize: 25,
+                color: Colors.yellow,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _PlayerStatsPhotos extends StatelessWidget {
+  const _PlayerStatsPhotos({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Image.asset(
+            "assets/goldexperiencestats.png",
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Expanded(
+          child: Image.asset(
+            "assets/killerqueenstats.png",
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _PlayersPhotos extends StatelessWidget {
+  const _PlayersPhotos({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Image.asset(
+            "assets/goldexperience.png",
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Expanded(
+          child: Image.asset(
+            "assets/killerqueen.png",
+            fit: BoxFit.fitHeight,
+          ),
         ),
       ],
     );
