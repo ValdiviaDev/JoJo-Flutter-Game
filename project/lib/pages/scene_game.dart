70ca7b1e69bb8a3c2a4895bd64493d11d3e8d114
David Valdivia <d.valdiviamartinez@gmail.com>
2020-01-17 17:02:30 +0100

    Starting game page layout

--- project/lib/pages/scene_game.dart
+++ project/lib/pages/scene_game.dart
@@ -46,21 +46,74 @@ class _SceneGameState extends State<SceneGame> {
             }
             return Column(
               children: <Widget>[
-                RaisedButton(
-                  child: Text('End Game'),
-                  onPressed: () {
-                    closing = true;
-                    lobbyRef.updateData({
-                      'Running': false,
-                    });
-                    Navigator.of(context).pop(false);
-                  },
+                Expanded(
+                  flex: 3,
+                  child: _PlayersAndStats(),
+                ),
+                Expanded(
+                   flex: 1,
+                  child: RaisedButton(
+                    child: Text('End Game'),
+                    onPressed: () {
+                      closing = true;
+                      lobbyRef.updateData({
+                        'Running': false,
+                      });
+                      Navigator.of(context).pop(false);
+                    },
+                  ),
                 ),
               ],
             );
           },
         ),
       ),
+    );
+  }
+}
+
+class _PlayersAndStats extends StatelessWidget {
+  const _PlayersAndStats({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+             Expanded(
+              child: Image.asset(
+                "assets/goldexperience.png",
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Expanded(
+              child: Image.asset(
+                "assets/killerqueen.png",
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+             Expanded(
+              child: Image.asset(
+                "assets/goldexperiencestats.png",
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Expanded(
+              child: Image.asset(
+                "assets/killerqueenstats.png",
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
